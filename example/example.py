3b9c1ee4689d47ca2ecd299ccf97b59809ab8cd9
Jose Daniel Hernandez <jsdaniel.h@gmail.com>
2022-06-26 21:03:59 -0600

    Update example code
    Update example code.

--- example/example.py
+++ example/example.py
@@ -1,22 +1,32 @@
 from nimiqclient import *
 
-# Create Nimiq RPC client
-client = NimiqClient(
-    scheme="http", user="luna", password="moon", host="127.0.0.1", port=8648
-)
 
-try:
-    # Get consensus
-    consensus = client.consensus()
-    print("Consensus: {0}".format(consensus))
+def main():
+    # Create Nimiq RPC client
+    client = NimiqClient(
+        scheme="http", user="luna", password="moon", host="127.0.0.1",
+        port=8648
+    )
 
-    if consensus == ConsensusState.ESTABLISHED:
-        # Get accounts
-        print("Getting basic accounts:")
-        for account in client.accounts():
-            if account.type == AccountType.BASIC:
-                # Show basic account address
-                print(account.address)
+    try:
+        # Get consensus
+        consensus = client.consensus()
+        print("Consensus: {0}".format(consensus))
 
-except InternalErrorException as error:
-    print("Got error when trying to connect to the RPC server: {0}".format(str(error)))
+        if consensus:
+            # Get accounts
+            print("Getting basic accounts:")
+            for address in client.accounts():
+                account = client.get_account_by_address(address)
+                if account.type == AccountType.BASIC:
+                    # Show basic account address
+                    print(account.address)
+
+    except InternalErrorException as error:
+        print(
+            "Got error when trying to connect to the RPC server: {0}".format(
+                str(error)))
+
+
+if __name__ == "__main__":
+    main()
